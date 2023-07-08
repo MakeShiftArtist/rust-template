ea7c14e7f7665a759657660e08c6202ac1a3ad27
makeshiftartist <47653736+MakeShiftArtist@users.noreply.github.com>
2023-07-07 22:02:17 -0500

    Chore: Add ./src/main.rs and logger

---
+++ src/bin/main.rs
@@ -0,0 +1,21 @@
+use env_logger::{Builder, Env};
+
+// Executable goes here
+fn main() {
+    get_logger();
+    log::info!("Improved logging is working!");
+}
+
+
+
+
+
+// This just sets up your logger. It should only be done once for the lifetime of the program
+fn get_logger() {
+    load_env();
+    Builder::from_env(Env::default().default_filter_or("warn")).init();
+}
+
+fn load_env() {
+    dotenv::dotenv().ok();
+}
